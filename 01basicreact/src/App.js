89183162a47d342b7d24f4c139bea1a5718eb69e
Abhishek <abhishekshukla3059@gmail.com>
2024-10-28 13:48:08 +0530

    added 01basicreact

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,13 @@
+
+import Hello from "./Hello";
+function App() {
+  return (
+    <>
+    <h1>This is Project created by Create react App which is outdated </h1>
+    <Hello />
+    </>
+   
+  );
+}
+
+export default App;
